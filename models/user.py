c11117b2755af06574dcbf6c9f05252333bef68f
MichaelKazembe <michaelkazembe@gmail.com>
2023-10-02 16:10:31 -0700

    security improvement

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import hashlib
 
 class User(BaseModel, Base):
     """Representation of a user """
@@ -27,3 +27,28 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """Getter for password"""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """Setter for password that hashes the value to MD5"""
+        if value:
+            self.__password = hashlib.md5(value.encode()).hexdigest()
+
+    def to_dict(self, hide_password=True):
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        if hide_password and "password" in new_dict:
+            del new_dict["password"]
+        return new_dict
